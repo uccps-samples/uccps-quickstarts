a0907c52f86fed671c699f386637a9af44ef1b8a
syxunion <86048991+syxunion@users.noreply.github.com>
2021-07-15 14:23:23 +0800

    Update MessageServlet.java

--- undertow-servlet/src/main/java/org/openshift/quickstarts/undertow/servlet/MessageServlet.java
+++ undertow-servlet/src/main/java/org/openshift/quickstarts/undertow/servlet/MessageServlet.java
@@ -16,7 +16,7 @@
  *  limitations under the License.
  */
 
-package org.utccp.quickstarts.undertow.servlet;
+package org.openshift.quickstarts.undertow.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
